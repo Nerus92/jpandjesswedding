838a592dea04c943f1ce0af198919cd93c15ac50
JP <jphilippe.chatain@gmail.com>
2024-11-09 15:38:07 -0800

    Address possible null refs

--- src/main.ts
+++ src/main.ts
@@ -121,13 +121,13 @@ const initApp = () => {
     const mobileMenu = document.getElementById('mobile-menu')
 
     const toggleMenu = () => {
-        mobileMenu.classList.toggle('hidden')
-        mobileMenu.classList.toggle('flex')
-        hamburgerBtn.classList.toggle('toggle-btn')
+        mobileMenu?.classList.toggle('hidden')
+        mobileMenu?.classList.toggle('flex')
+        hamburgerBtn?.classList.toggle('toggle-btn')
     }
 
-    hamburgerBtn.addEventListener('click', toggleMenu)
-    mobileMenu.addEventListener('click', toggleMenu)
+    hamburgerBtn?.addEventListener('click', toggleMenu)
+    mobileMenu?.addEventListener('click', toggleMenu)
 }
 
 document.addEventListener('DOMContentLoaded', initApp)
